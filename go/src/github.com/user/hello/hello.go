779e16418236fe093206883ae0b3c8b10d2391c6
Morgan Willis <gangstanizzerd@gmail.com>
2017-10-27 10:04:00 -0400

    corrected the Hello.go program

---
+++ go/src/github.com/user/hello/hello.go
@@ -0,0 +1,12 @@
+// Author: Morgan M Willis
+//Project: Terraform Nagrom Test Program in GO
+//Date" 10/27/2017
+//
+
+package main
+
+import "fmt"
+
+func main() {
+         fmt.Printf("Hello, world.\n")
+}
